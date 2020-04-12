30df48a9caa353ef4baecd4625c5400b78616392
Alex <alexeek@protonmail.com>
2020-04-12 08:08:29 +0100

    🔥 Unused import

--- angular/src/app/api.service.ts
+++ angular/src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AppConfiguration } from 'src/configuration';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
